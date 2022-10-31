bacaa677e89dbbd1bbd880d9e81a457e69bcba4c
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-10-31 23:16:19 +0200

    Completes tasks 1,2 HW 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryWrapper = document.querySelector("div.gallery");
+
+let htmlString = "";
+galleryItems.forEach(element => {
+    htmlString+=
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+        <img
+            class="gallery__image"
+            src="${element.preview}"
+            data-source="${element.original}"
+            alt="${element.description}"
+        />
+        </a>
+    </div>`
+});
+galleryWrapper.insertAdjacentHTML("afterbegin", htmlString);
+
+galleryWrapper.addEventListener("click", callBasicLightbox); 
+
+function callBasicLightbox(event)
+{
+
+    function addESClistener(event) {
+        if (event.code==="Escape") {lb.close(()=>document.removeEventListener("keydown", addESClistener))}
+    }
+    
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return "";
+
+    const lb = basicLightbox.create(`<img src=${event.target.dataset.source}>`);
+    lb.show(()=>document.addEventListener("keydown", addESClistener));
+      
+}
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryWrapper = document.querySelector("ul.gallery");
+
+let htmlString = "";
+galleryItems.forEach(element => {
+    htmlString+=
+    `<li><a class="gallery__link" href="${element.original}">
+        <img
+            class="gallery__image"
+            src="${element.preview}"
+            alt="${element.description}"
+        />
+    </a></li>`
+});
+galleryWrapper.insertAdjacentHTML("afterbegin", htmlString);
+
+let lightbox = new SimpleLightbox('.gallery a', { captions: true, captionsData: "alt", captionDelay: 250 });
+lightbox.on('show.simplelightbox');
+
+
 console.log(galleryItems);
